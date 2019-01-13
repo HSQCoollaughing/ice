dc521d957382e20f5fd2bdc1ae93e8f29a27c36b
HSQCoollaughing <1070404612@qq.com>
2019-01-13 20:53:19 +0800

    创建菜单
    注意数据格式

--- server/wechat/menu.js
+++ server/wechat/menu.js
@@ -1,25 +1,30 @@
 export default {
-    button: [{
-      'name': '权游周边',
-      'sub_button': [{
-        'name': '最新种子',
-        'type': 'click',
-        'key': 'bt'
-      }, {
-        'name': '手办商城',
-        'type': 'view',
-        'key': 'https://iceandfire.iblack7.com/shopping'
+  "button":[
+    {    
+         "type":"click",
+         "name":"今日歌曲",
+         "key":"V1001_TODAY_MUSIC"
+     },
+     {
+          "name":"菜单",
+          "sub_button":[
+          {    
+              "type":"view",
+              "name":"搜索",
+              "url":"http://www.soso.com/"
+           },
+           {
+                "type":"miniprogram",
+                "name":"wxa",
+                "url":"http://mp.weixin.qq.com",
+                "appid":"wx286b93c14bbf93aa",
+                "pagepath":"pages/lunar/index"
+            },
+           {
+              "type":"click",
+              "name":"赞一下我们",
+              "key":"V1001_GOOD"
+           }]
       }]
-    }, {
-      'name': '冰火家族',
-      'type': 'view',
-      'url': 'https://iceandfire.iblack7.com'
-    }, {
-       'type': 'miniprogram',
-       'name': '权游小程序',
-       'url': 'https://iceandfire.iblack7.com',
-       'appid': 'wxcac851ec7e9550e2',
-       'pagepath': 'pages/index'
-   }]
   }
   
